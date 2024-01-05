9d2d278d73c96c659ec4bdb2116a856ff49edaa7
Arnaldo Manita <155696598+arnaldomanita@users.noreply.github.com>
2024-01-04 23:26:23 -0300

    feat: adicionando solução program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia: ");
+        Nokia nokia = new Nokia("998854587", "Nokia Xr", "123456", 128);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone: ");
+        Iphone iphone = new Iphone("988554672", "Iphone 11", "654321", 64);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+    }
+}
